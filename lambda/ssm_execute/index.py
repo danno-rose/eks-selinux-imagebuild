3eb72088f01645c7da802f098761e029dbcfc846
Daniel Rose <drosea@amazon.co.uk>
2020-01-20 15:05:37 +0000

    git ignore

--- lambda/ssm_execute/index.py
+++ lambda/ssm_execute/index.py
@@ -8,10 +8,12 @@ from operator import itemgetter
 
 SSMDocument = os.environ['ssm_doc']
 #SearchString = os.environ['amiSearchString']
+
+
 def lambda_handler(event, context):
-   #yeardaymonthtime =datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-   #val = str(yeardaymonthtime)
-   client = boto3.client('ec2')
+    #yeardaymonthtime =datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    #val = str(yeardaymonthtime)
+    client = boto3.client('ec2')
 #   response = client.describe_images(
 #     Filters=[{
 #         'Name': 'name',
@@ -27,15 +29,15 @@ def lambda_handler(event, context):
 #         'amazon'
 #     ]
 #   )
-   # Sort on Creation date Desc
-   #image_details = sorted(response['Images'], key=itemgetter('CreationDate'), reverse=True)
-   ami_id = "ami-03772b2e67db0c87b"
-   #Send Parameters to SSM to start job
-   client = boto3.client('ssm')
-   response = client.start_automation_execution(
-    DocumentName= (SSMDocument),
-   Parameters={
-     #"AMIVersion":[val],
-      "amiId":[ami_id]
-      }
-   )
+    # Sort on Creation date Desc
+    #image_details = sorted(response['Images'], key=itemgetter('CreationDate'), reverse=True)
+    ami_id = "ami-03772b2e67db0c87b"
+    # Send Parameters to SSM to start job
+    client = boto3.client('ssm')
+    response = client.start_automation_execution(
+        DocumentName=(SSMDocument),
+        Parameters={
+            # "AMIVersion":[val],
+            "amiId": [ami_id]
+        }
+    )
